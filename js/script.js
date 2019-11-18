1590151813e91e15e6e53750b36cb8c0dc012711
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-11-18 11:23:09 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,32 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 
 var canvas = document.getElementById('ICanvas');
-
+canvas.addEventListener('click',onClick);
 canvas.width = width;
 canvas.height = height;
 
-var Event = {
-     
+
+function getMousePosition(event) {
+    var x = y = 0,
+        doc = document.documentElement,
+        body = document.body;
+    if(!event) event=window.event;
+    if (window.pageYoffset) {
+        x = window.pageXOffset;
+        y = window.pageYOffset;
+    }else{
+        x = (doc && doc.scrollLeft || body && body.scrollLeft || 0)
+          - (doc && doc.clientLeft || body && body.clientLeft || 0);
+        y = (doc && doc.scrollTop  || body && body.scrollTop  || 0)
+          - (doc && doc.clientTop  || body && body.clientTop  || 0);
+    }
+    x += event.clientX;
+    y += event.clientY;
+    return [x, y];
+}
+
+function onClick(event) {
+    console.log(getMousePosition(event));
 }
 
 function Color(r, g, b, a) {
@@ -76,6 +96,7 @@ function Button(x, y, w, h) {
     var component = new Component(x, y, w, h);
     return component;
 }
+
 function Lable(x, y, w, h, t) {
     var component = new Component(x, y, w, h);
     component.fontcolor = Color(0, 0, 0, 1);
@@ -89,7 +110,7 @@ function Lable(x, y, w, h, t) {
 }
 
 var frame = new Frame();
-var bg = new Component(15, 15, width - 15, 1800);
+var bg = new Component(0, 0, width, height);
 bg.backgroundcolor = Color(125, 55, 55, 1);
 bg.add(new Button(55, 55, 333, 333));
 bg.add(new Lable(55, 55, 30, 30,"大家好啊"));
